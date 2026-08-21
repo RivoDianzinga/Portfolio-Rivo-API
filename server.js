41c5a3d8f853c956c6850a388b8a457429c635e1
Rivo Dianzinga <dmamyrivo@gmail.com>
2026-08-21 09:42:33 +0200

    Ajout de l'authentification administrateur avec JWT

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config(); // charge explicitement .env pour utiliser directement 
+// les variables de postgresql (db.js) et les variables d'administration (.env)
 const express = require("express"); // on charge Express
 const cors = require("cors"); // on charge le middleware CORS permettant d'activer le partage
 // des ressources entre différents origines, notamment entre le front-end et le back-end
@@ -5,7 +7,12 @@ const cors = require("cors"); // on charge le middleware CORS permettant d'activ
 //console.log(publications); // affiche dans le terminal
 const pool = require("./db");
 const app = express(); // on créé l'application serveur
+const bcrypt = require("bcryptjs"); // permet de hasher et vérifier le mpd sans les stocker en clair
+const jwt = require("jsonwebtoken"); // permet de créer une preuve temporaire d'authentification
 app.use(cors()); // function CORS qui autorise toutes les origines
+app.use(express.json()); // utilisé pour l'interface d'administration séparée que les 
+// recruteurs ne voient pas. cette interface aura besoin de l'email et du mot de passe
+// dans un fichier json
 //const port = 3000;     // définition du port pour le localhost c-à-d mon pc
 const port = process.env.PORT || 3000; // localement, on continue à avoir localhost : 3000
 // mais sur Render, c'est l'hébergeur qui fournit le port à utiliser via la variable PORT, 
@@ -25,6 +32,68 @@ const port = process.env.PORT || 3000; // localement, on continue à avoir local
 app.get("/", function (requete, reponse) {   // quand le navigateur fait une requète HTTP
 // GET/, Express exécute cette fonction et renvoie une réponse --> principe du routage Express
   reponse.send("API Portfolio Rivo opérationnelle"); // ici, l'api renvoie une page html avec une phrase
+});
+
+/*
+Ici on définit la 1è route d'authentification POST /api/admin/login, l'API qui permet
+de poster, d'ajouter, d'insérer de nouvelles données de publications. Cette API est
+sécurisée, contrairement à l'API GET ci-dessous qui permet de lire publiquement 
+les données de publications.
+*/
+app.post("/api/admin/login", async function(requete, reponse) { 
+// async signie que cette function va effectuer des opérations asynchrones
+    const email = requete.body.email;
+    const password = requete.body.password;
+    // Vérification que les deux informations ont bien été envoyées
+//    console.log("ADMIN_EMAIL chargé :",Boolean(process.env.ADMIN_EMAIL));
+//    console.log("ADMIN_PASSWORD_HASH chargé :",Boolean(process.env.ADMIN_PASSWORD_HASH));
+//    console.log("Email correspondant :",email === process.env.ADMIN_EMAIL);    
+    if (!email || !password) {
+        return reponse.status(400).json({
+            error: "Email et mot de passe requis"
+        });
+    }
+    // Vérification de l'identifiant administrateur
+    if (email !== process.env.ADMIN_EMAIL) {
+        return reponse.status(401).json({
+            error: "Identifiants incorrects"
+        });
+    }
+    try {
+        // Comparaison du mot de passe saisi avec le hash stocké dans .env
+        const motDePasseValide = await bcrypt.compare(
+// await signifie attendre le résultat de cette opération avant de poursuivre la fonction
+            password,
+            process.env.ADMIN_PASSWORD_HASH
+        );
+        if (!motDePasseValide) {
+            return reponse.status(401).json({
+                error: "Identifiants incorrects"
+            });
+        }
+        // L'identité est correcte : création d'un JWT
+        const token = jwt.sign(
+            {
+                role: "admin"
+            },
+            process.env.JWT_SECRET, // le token expire 1j après
+            {
+                expiresIn: "1h"
+            }
+        );
+        reponse.json({
+            message: "Authentification réussie",
+            token: token
+        });
+    } catch (erreur) {
+        console.error(
+            "Erreur lors de l'authentification :",
+            erreur
+        );
+        reponse.status(500).json({
+            error: "Erreur interne du serveur"
+        });
+    }
 });
 
 //app.get("/api/publications", function(requete,reponse){
